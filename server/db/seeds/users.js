73dc57601f07a12ae5f15914bcd8953d72e4681b
student <student@eda-bc-09.localdomain>
2021-02-25 09:55:41 +1300

    seeds done

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -5,9 +5,11 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, username: 'superste63', first_name:'Stephen', last_name:' Perry', password:'stephen'},
+        {id: 2, username: 'samar1990', first_name:'Samardeep', last_name: 'Gill', password:'samardeep'},
+        {id: 3, username: 'poonam89', first_name:'Poonam', last_name: 'Sharma', password:'poonam'},
+        {id: 4, username: 'booby63', first_name:'Booby', last_name:' Grey', password:'booby'},
+        {id: 5, username: 'john2016', first_name:'John', last_name:' Smith', password:'john'}
       ]);
     });
 };
