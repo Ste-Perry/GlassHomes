75226926f4b1e3729fe6434d241852dd0b527b06
student <student@eda-bc-09.localdomain>
2021-02-25 11:46:04 +1300

    Add review and property. also get by id.

--- server/db/reviews.js
+++ server/db/reviews.js
@@ -4,6 +4,18 @@ function getReviews (db = connection) {
   return db('reviews').select()
 }
 
+function addReview (reviews, db = connection) {
+  return db('reviews')
+  .insert(reviews)
+  .then(ids => ids[0])
+}
+function getReviewById (id, db = connection) {
+  return db('reviews')
+  .where('id',id)
+  .first()
+}
 module.exports = {
-  getReviews
+  getReviews,
+  addReview,
+  getReviewById
 }
--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,3 +1,4 @@
+const routes = require('authenticare/server/routes')
 const express = require('express')
 
 const db = require('../db/reviews')
@@ -14,6 +15,34 @@ router.get('/', (req, res) => {
       console.log(err)
       res.status(500).json({ message: 'Somthing went wrong' })
     })
+})
+
+router.get ('/:id', (req, res) =>{
+  reviewId = req.params.id
+  db.getReviewById(reviewId)
+  .then(id => {
+    res.json (id)
+    })
+  })
+
+router.post('/', (req,res) => {
+  const review = {
+    title: req.body.title,
+    comments: req.body.comments,
+    pros: req.body.pros,
+    cons: req.body.cons,
+    rating: req.body.rating,
+    year_of_tenancy: req.body.year_of_tenancy
+  }
+  console.log(req.body)
+  db.addReview(review)
+  .then(review => {
+    res.json({ review: review })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({ message: 'Post review broken' })
+  })
 })
 
 module.exports = router
