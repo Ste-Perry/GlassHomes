81c401d58a4b50b284b3da23e2e28bf3dffc911d
tom-may <tommay90@gmail.com>
2021-02-26 11:32:43 +1300

    add side banners

--- client/components/Properties.jsx
+++ client/components/Properties.jsx
@@ -13,49 +13,52 @@ function Properties(props) {
 
 	return (
 		<>
-     <section className="hero is-info is-medium is-bold" style={{ backgroundImage: 'url(/images/vic.jpg)' }}>
-        <div className="hero-body"></div>
-      </section>
-       
-      <ul>
-			<section className="articles">
-        <div className="column is-8 is-offset-2">
-        <div className="container has-text-centered">
-        <div className="card article">
-              <div className="card-content">
-                
-							<div className="column is-12">
-              <h3 className="title has-text-black">Properties</h3>
-								<hr className="login-hr"></hr>
-                <p className="subtitle has-text-black">Have a looksie.</p>
+			<section className="hero is-info is-medium is-bold" style={{ backgroundImage: 'url(/images/vic.jpg)' }}>
+				<div className="hero-body"></div>
+			</section>
 
-				{props.properties.map(prp => {
-					return (
-						<Link to={`/property/${prp.id}`}>
-          <div className="card article">
-            <div className="card-content">
-              <div className="media">
-                <div className="media-content has-text-centered">
+			<ul>
+				<section className="articles">
+					<div className="column is-8 is-offset-2">
+						<div className="container has-text-centered">
+							<div className="card article">
+								<div className="card-content">
 
-														<p className="title article-title">{prp.address}</p>
-												<li key={prp.id}>Address: {prp.suburb} {prp.address} Bedrooms: {prp.bedrooms} Bathrooms: {prp.bathrooms} Parking spaces: {prp.parking}</li>
+									<div className="column is-12">
+										<h3 className="title has-text-black">Properties</h3>
+										<hr className="login-hr"></hr>
+										<p className="subtitle has-text-black">Have a looksie.</p>
+
+										{props.properties.map(prp => {
+											return (
+												<Link to={`/property/${prp.id}`}>
+													<div className="card article">
+														<div className="card-content">
+															<div className="media">
+																<div className="media-content has-text-centered">
+
+																	<p className="title article-title">{prp.address}</p>
+																	<li key={prp.id}>Address: {prp.suburb} {prp.address} Bedrooms: {prp.bedrooms} Bathrooms: {prp.bathrooms} Parking spaces: {prp.parking}</li>
+																</div>
+															</div>
+														</div>
+													</div>
+													<br></br>
+													<br></br>
+												</Link>
+
+											)
+										})}
+									</div>
 								</div>
-              </div>
-            </div>
-          </div>
-					<br></br>
-					<br></br>
-						</Link>
-					
-					)
-				})}
-        </div>
-                    </div>
-           </div>
-           </div>
-            </div>
+							</div>
 
-         </section>
+						</div>
+					</div>
+
+					<div className="advert-banner-left"><img src="https://picsum.photos/800/600/?random" alt="Description" style={{"height" : "100%"}}/></div>
+					<div className="advert-banner-right"><img src="https://picsum.photos/800/600/?random" alt="Description" style={{"height" : "100%"}}/></div>
+				</section>
 
 
 			</ul>
