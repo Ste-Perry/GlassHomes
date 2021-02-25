611151c68bdcaba72ad8135fc35ad86347bbeb44
student <student@eda-bc-09.localdomain>
2021-02-25 14:15:28 +1300

    added internal api

---
+++ client/apis/reviews.js
@@ -0,0 +1,36 @@
+import request from 'superagent'
+
+const rootUrl = '/api/v1/reviews'
+
+export function getReviews () {
+  return request.get(rootUrl )
+    .then(res => {
+      return res.body.reviews
+    })
+}
+
+export function getReviewById (id) {
+  return request.get(rootUrl + id)
+  .then(res => res.body)
+}
+
+export function addReview (){
+  return request.post(rootUrl)
+  .send ({
+    title: title,
+    comments:comments,
+    pros:pros,
+    cons:cons,
+    rating:rating,
+    year_of_tenancy:year_of_tenancy
+  })
+  .then(res => {
+    return res.body
+  })
+}
+
+export function deleteReview(){
+  return request.delete(rootUrl)
+  .send(review)
+  .then(res => res.body)
+}
