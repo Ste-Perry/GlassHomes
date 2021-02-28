c91377114d7e93a0d7301359cec085f0a2ed29ed
tom-may <tommay90@gmail.com>
2021-02-28 13:26:09 +1300

    fix footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function Footer () {
+function Footer() {
 
-    return (
-        <>
-            <footer class="footer">
-  <div class="content has-text-centered">
-    <p>
-      <strong>GlassHomes </strong> by Team Yeet . Developed at
-      <a href="https://devacademy.co.nz"> Dev Academy</a>.
-    </p>
-		<div>
-    <p>
-		<Link to="/loginadmin">Admin</Link> /   
-		<a href="mailto:admin@glasshomes.co.nz"> Contact</a> / 
-		<Link to="adminportal"> Admin Portal (will remove)</Link>
-		</p>
-		</div>
-  </div>
-</footer>
-        </>
-    )
+	return (
+		<>
+			<footer class="footer">
+				<div class="content has-text-centered">
+					<p>
+						<strong>GlassHomes </strong> by Team Yeet . Developed at
+      			<a href="https://devacademy.co.nz"> Dev Academy</a>.
+   				</p>
+					<div>
+						<p >
+							<Link to="/loginadmin">Admin</Link> &nbsp;·&nbsp;
+							<a href="mailto:admin@glasshomes.co.nz"> Contact</a> &nbsp;·&nbsp;
+							<Link to="adminportal"> Admin Portal (will remove)</Link>
+						</p>
+					</div>
+				</div>
+			</footer>
+		</>
+	)
 }
 
 export default Footer
