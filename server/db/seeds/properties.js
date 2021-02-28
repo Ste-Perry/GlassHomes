0df2efa0fa4e82277f41ae31d8398a68872fd5be
jeff-seymour <mojosodomo@gmail.com>
2021-03-01 09:29:47 +1300

    addedseeds

--- server/db/seeds/properties.js
+++ server/db/seeds/properties.js
@@ -14,7 +14,50 @@ exports.seed = function(knex) {
         {id: 7, suburb: 'Mount Victoria', address: '20 Queen Street', bedrooms: 3, bathrooms: 1, parking: 2,avg_score: 5, img: ''},
         {id: 8, suburb: 'Mount Victoria', address: '20 Elizabeth Street', bedrooms: 3, bathrooms: 1, parking: 2,avg_score: 5, img: ''},
         {id: 9, suburb: 'Woburn', address: '14 St Albans Grov', bedrooms: 1, bathrooms: 1, parking: 2,avg_score: 5, img: ''},
-        {id: 10, suburb: 'Newtown', address: '10 Green Street', bedrooms: 2, bathrooms: 1, parking: 0,avg_score: 5, img: ''}
+        {id: 10, suburb: 'Newtown', address: '10 Green Street', bedrooms: 2, bathrooms: 1, parking: 0,avg_score: 5, img: ''},
+
+        {id: 11, suburb: 'Churton Park', address: '15 Furlong Crescent', bedrooms: 4, bathrooms: 3, parking: 4,avg_score: 5, img: ''},
+        {id: 12, suburb: 'Featherston', address: '110 Brandon Street', bedrooms: 3, bathrooms: 1, parking: 20,avg_score: 5, img: ''},
+        {id: 13, suburb: 'Taradale', address: '115a Guppy Road', bedrooms: 3, bathrooms: 1, parking: 3,avg_score: 5, img: ''},
+        {id: 14, suburb: 'Khandallah', address: '32 Torwood Road', bedrooms: 4, bathrooms: 3, parking: 2,avg_score: 5, img: ''},
+        {id: 15, suburb: 'Newlands', address: '54 Colchester Crescent', bedrooms: 5, bathrooms: 2, parking: 5,avg_score: 3, img: ''},
+        {id: 16, suburb: 'Johnsonville', address: '16a Ceres Crescent', bedrooms: 2, bathrooms: 1, parking: 1,avg_score: 2, img: ''},
+        {id: 17, suburb: 'Churton Park', address: '72 Melksham Drive', bedrooms: 5, bathrooms: 3, parking: 4,avg_score: 5, img: ''},
+        {id: 18, suburb: 'Te Aro', address: '13 Garrett Street', bedrooms: 6, bathrooms: 1, parking: 0,avg_score: 0, img: ''},
+        {id: 19, suburb: 'Churton Park', address: '65 Abilene Crescent', bedrooms: 4, bathrooms: 3, parking: 2,avg_score: 5, img: ''},
+        {id: 20, suburb: 'Te Aro', address: '21 Marion Street', bedrooms: 4, bathrooms: 1, parking: 0,avg_score: 0, img: ''},
+
+        {id: 21, suburb: 'Te Aro', address: '101 Manners Street', bedrooms: 5, bathrooms: 2, parking: 0,avg_score: 1, img: ''},
+        {id: 22, suburb: 'Te Aro', address: '376 The Terrace', bedrooms: 4, bathrooms: 2, parking: 2,avg_score: 2, img: ''},
+        {id: 23, suburb: 'Berhampore', address: '2 Luxford Street', bedrooms: 5, bathrooms: 2, parking: 1,avg_score: 0, img: ''},
+        {id: 24, suburb: 'Riccarton', address: '81a Rattray Street', bedrooms:4 , bathrooms: 1, parking: 2,avg_score: 2, img: ''},
+        // {id: 25, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 26, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 27, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 28, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 29, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 30, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''}
+
+        // {id: 31, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 32, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 33, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 34, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 35, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 36, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 37, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 38, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 39, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 40, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 41, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 42, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 43, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 44, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 45, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 46, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 47, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 48, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 49, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''},
+        // {id: 50, suburb: '', address: '', bedrooms: , bathrooms: , parking: ,avg_score: , img: ''}
       ])
     })
 };
--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -9,7 +9,25 @@ exports.seed = function(knex) {
         {id: 2, username: 'samar1990', first_name:'Samardeep', last_name: 'Gill', password:'samardeep'},
         {id: 3, username: 'poonam89', first_name:'Poonam', last_name: 'Sharma', password:'poonam'},
         {id: 4, username: 'booby63', first_name:'Booby', last_name:' Grey', password:'booby'},
-        {id: 5, username: 'john2016', first_name:'John', last_name:' Smith', password:'john'}
+        {id: 5, username: 'john2016', first_name:'John', last_name:' Smith', password:'john'},
+
+        {id: 6, username: 'bigJ99', first_name:'Richard', last_name:'Johnson', password:'Richard'},
+        {id: 7, username: 'fabalicious', first_name:'Fabio', last_name: 'Lanzoni', password:'Fabio'},
+        {id: 8, username: 'jimmy3K', first_name:'James', last_name: 'Beam', password:'james'},
+        {id: 9, username: 'crateboi', first_name:'Carlos', last_name:'Ghosn', password:'carlos'},
+        {id: 10, username: 'milanit247', first_name:'Giorgio', last_name:'Armani', password:'giorgio'},
+        
+        {id: 11, username: 'westlyf', first_name:'Kanye', last_name:'Best', password:'kanye'},
+        {id: 12, username: 'sUpErSiCk', first_name:'Ash', last_name: 'Bloomfield', password:'ash'},
+        {id: 13, username: 'MiniMan', first_name:'Austin', last_name: 'Minor', password:'austin'},
+        {id: 14, username: 'Awkward88', first_name:'Nora', last_name:'Lum', password:'nora'},
+        {id: 15, username: 'PininIt', first_name:'Battista', last_name:'Farina', password:'battista'},
+
+        {id: 16, username: 'laid-back', first_name:'Isatta', last_name:'Sheriff', password:'isatta'},
+        {id: 17, username: 'DOOM20', first_name:'Dan', last_name:'Dumile', password:'daniel'},
+        {id: 18, username: 'stnawalol', first_name:'Sarah', last_name:'Vaughan', password:'sarah'},
+        {id: 19, username: 'chassin', first_name:'Masayo', last_name:'Minowa', password:'masayo'},
+        {id: 20, username: 'Special', first_name:'Som', last_name:'Sang', password:'som'}
       ]);
     });
 };
