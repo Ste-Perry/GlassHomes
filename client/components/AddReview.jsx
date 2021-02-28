4476a5a9ee83c8bc1ee00960b6b0020cc730f510
tom-may <tommay90@gmail.com>
2021-03-01 09:38:59 +1300

    fixed oopisies reviews

--- client/components/AddReview.jsx
+++ client/components/AddReview.jsx
@@ -141,7 +141,7 @@ console.log(formData)
                       </label>
                     </div>
                   </div>
-    <div>                 </label>
+    <div>                
                 
                 <label className="" htmlFor="img">
                   <span className="">Review image </span>
@@ -219,16 +219,6 @@ console.log(formData)
     //                 <input className='form' type='text' name='start_of_tenancy' placeholder='Start_of_Tenancy' onChange={(e) => handleChange(e)}/>
 
 
-                </label>
-                
-                <label className="" htmlFor="img">
-                  <span className="">Review image </span>
-                   <input type="file" name="img" onChange={onChangeFile} />
-               </label>
-                <button type='submit'>Add</button>
-            </form>
-            
-    </>
   )
 }
 
