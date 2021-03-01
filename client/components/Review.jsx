09eec7caa178698329aa50e546ef12f60b1c9871
student <student@eda-bc-08.localdomain>
2021-03-01 14:40:32 +1300

    add review photo and view photo

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import React, {useEffect, useState} from "react";
+import {connect} from "react-redux";
 
 const Review = (props) => {
   const review = props.review;
@@ -9,49 +9,71 @@ const Review = (props) => {
     let remainingStars = 5 - review.rating;
     for (let i = 0; i < review.rating; i++) {
       starArray.push(
-        <span style={{ color: "gold" }} className="fa fa-star"></span>
+        <span style={{color: "gold"}} className="fa fa-star"></span>
       );
     }
     for (let i = 0; i < remainingStars; i++) {
       starArray.push(<span className="fa fa-star"></span>);
     }
-    return starArray
-  }
+    return starArray;
+  };
+
+  const [showImg, setShowImg] = useState(false);
 
   return (
-    <div key={review.id}  >
-    
-          <div className="card article">
-            <div className="card-content">
-              <div className="media">
-                <div className="media-content has-text-centered">
-                  <p className="title article-title">{review.title}</p>
-                  <br />
-                  <p className="years_of_tenancy">
-                    Years of tenancy: {review.start_of_tenancy} - {review.end_of_tenancy}
-                  </p>
-             
-                  <br />
-                  <div className="icon-text has-text-success"><i className="fa fa-check-square"></i><span> Pros:</span></div>
-                  <p>{review.pros}</p>
-                  <br />
-                  <div className="icon-text has-text-danger"><i className="fa fa-ban"></i><span> Cons:</span></div>
-                  <p>{review.cons}</p>
-                  <br />
-                  <p className="comments">Comments: {review.comments}</p>
-                  <br />
-                  Rating: {stars()}
-                  <br />
-                  <br />
-                  <button className="button is-info">Helpful</button>
-                </div>
+    <div key={review.id}>
+      <div className="card article">
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content has-text-centered">
+              <p className="title article-title">{review.title}</p>
+              <br />
+              <p className="years_of_tenancy">
+                Years of tenancy: {review.start_of_tenancy} -{" "}
+                {review.end_of_tenancy}
+              </p>
+              <br />
+              <div className="icon-text has-text-success">
+                <i className="fa fa-check-square"></i>
+                <span> Pros:</span>
               </div>
+              <p>{review.pros}</p>
+              <br />
+              <div className="icon-text has-text-danger">
+                <i className="fa fa-ban"></i>
+                <span> Cons:</span>
+              </div>
+              <p>{review.cons}</p>
+              <br />
+              <p className="comments">Comments: {review.comments}</p>
+              <br />
+              Rating: {stars()}
+              <br />
+              <br />
+              {review.img && (
+                <button
+                  onClick={() => setShowImg(!showImg)}
+                  className="button is-success"
+                >
+                  See Home Pics
+                </button>
+              )}
+              <br />
+              <br />
+              {showImg && (
+                <img className="review-img" alt="uh oh!" src={review.img} />
+              )}
+              <br />
+              <br />
+              <button className="button is-info">Helpful</button>
             </div>
           </div>
-          <br></br>
-          <br></br>
         </div>
-        
+      </div>
+      <br></br>
+      <br></br>
+    </div>
+
     //   </section>
     // </div>
   );
