9f6965d1793cd509570dabbdbc83b76d18dd1e76
student <student@eda-bc-11.localdomain>
2021-03-03 14:59:17 +1300

    logoisButton

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -24,8 +24,11 @@ function Nav({ auth, logout, fetchHouses }) {
     <div className="container">
       <div className="navbar-brand">
         
-      <div className="navbar-item">
-      <img src='/images/default-monochrome.svg' width='200' height="28" />
+      <div className="navbar-item" >
+      <Link to="/home">
+      <img src='/images/default-monochrome.svg' width='200' height="28" href='/home'/>
+      </Link>
+      
       </div>
         <span onClick={toggleBurger} className={`navbar-burger burger ${burgerVisible ? 'is-active' : ''}`} data-target="navbarMenuHeroA">
           <span></span>
